5517575867f94d16312919241da43cd32b406125
Rakshit honnavar <rakshithonnavar20@gmail.com>
2023-10-08 00:44:16 +0530

    OIBSIP changes

--- script.js
+++ script.js
@@ -1,21 +1,68 @@
-let inputBox = document.getElementById('inputBox')
-let buttons  = document.querySelector('button')
+document.addEventListener("DOMContentLoaded", function () {
+    const inputBox = document.getElementById("inputBox");
+    let resultDisplayed = false;  // Flag to track whether result is displayed or not
 
-let string = ''
+    // Function to update the input box
+    function updateInput(value) {
+        if (resultDisplayed) {
+            inputBox.value = "";
+            resultDisplayed = false;
+        }
+        inputBox.value += value;
+    }
 
-buttons.forEach(element =>{
-     element.addEventListener('click',(b)=>{
-         if(b.target.innerText == "="){
-            string = String(eval(string))
-            inputBox.value = string;
-     }
-       else if(b.target.innerText == 'AC')
-       {
-        string = ''
-        inputBox.value = string;
-       }
-       else if(b.target.innerText =='DEL')
-       {
-        string  = string.substring(0,string.length-1)
-        inputBox.value = string;
-       }
+    
+
+    // Event listener for button clicks
+    document.querySelectorAll("button").forEach((button) => {
+        button.addEventListener("click", (event) => {
+            const buttonValue = event.target.innerText;
+
+            if (buttonValue === "AC") {
+                inputBox.value = "0";
+            } else if (buttonValue === "DEL") {
+                inputBox.value = inputBox.value.slice(0, -1);
+            } else if (buttonValue === "CLEAR") {
+                inputBox.value = "";
+            } else if (buttonValue === "=") {
+                try {
+                    inputBox.value = eval(inputBox.value);
+                    resultDisplayed = true;
+                } catch (error) {
+                    inputBox.value = "Error";
+                    resultDisplayed = true;
+                }
+            } else {
+                updateInput(buttonValue);
+            }
+        });
+    });
+});
+
+let inputBox = document.getElementById('inputBox');
+
+function appendValue(value) {
+    inputBox.value += value;
+}
+
+function clearDisplay() {
+    inputBox.value = '';
+}
+
+function deleteLast() {
+    inputBox.value = inputBox.value.slice(0, -1);
+}
+
+function calculate() {
+    let result = eval(inputBox.value);
+    inputBox.value = result;
+}
+
+function calculateSquareRoot() {
+    let inputValue = parseFloat(inputBox.value);
+    if (!isNaN(inputValue)) {
+        inputBox.value = Math.sqrt(inputValue);
+    } else {
+        inputBox.value = 'Invalid Input';
+    }
+}
