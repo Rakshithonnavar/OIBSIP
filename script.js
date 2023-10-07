b1b04d1456d357df2fed3d32f07fa78db87deb75
Rakshit honnavar <rakshithonnavar20@gmail.com>
2023-10-07 20:40:15 +0530

    OIBSIP changes

---
+++ script.js
@@ -0,0 +1,21 @@
+let inputBox = document.getElementById('inputBox')
+let buttons  = document.querySelector('button')
+
+let string = ''
+
+buttons.forEach(element =>{
+     element.addEventListener('click',(b)=>{
+         if(b.target.innerText == "="){
+            string = String(eval(string))
+            inputBox.value = string;
+     }
+       else if(b.target.innerText == 'AC')
+       {
+        string = ''
+        inputBox.value = string;
+       }
+       else if(b.target.innerText =='DEL')
+       {
+        string  = string.substring(0,string.length-1)
+        inputBox.value = string;
+       }
