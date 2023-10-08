f367d1420701717b3d8ec473d85aee98898691fe
Rakshit honnavar <rakshithonnavar20@gmail.com>
2023-10-08 16:32:30 +0530

    OIBSIP changes

--- script.js
+++ script.js
@@ -1,68 +1,77 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const inputBox = document.getElementById("inputBox");
-    let resultDisplayed = false;  // Flag to track whether result is displayed or not
-
-    // Function to update the input box
-    function updateInput(value) {
-        if (resultDisplayed) {
-            inputBox.value = "";
-            resultDisplayed = false;
-        }
-        inputBox.value += value;
+document.getElementById('form-Task').addEventListener('submit', saveTask);
+ 
+// Save new To-Do
+function saveTask(e) {
+ 
+  let title = document.getElementById('title').value;
+  let description = document.getElementById('description').value;
+ 
+ 
+  let task = {
+    title,
+    description
+  };
+ 
+  if (localStorage.getItem('tasks') === null) {
+    let tasks = [];
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  } else {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+ 
+  getTasks();
+ 
+  // Reset form-Task
+  document.getElementById('form-Task').reset();
+  e.preventDefault();
+ 
+}
+ 
+// Delete To-Do 
+function deleteTask(title) {
+ 
+  let tasks = JSON.parse(localStorage.getItem('tasks'));
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].title == title) {
+      tasks.splice(i, 1);
     }
-
-    
-
-    // Event listener for button clicks
-    document.querySelectorAll("button").forEach((button) => {
-        button.addEventListener("click", (event) => {
-            const buttonValue = event.target.innerText;
-
-            if (buttonValue === "AC") {
-                inputBox.value = "0";
-            } else if (buttonValue === "DEL") {
-                inputBox.value = inputBox.value.slice(0, -1);
-            } else if (buttonValue === "CLEAR") {
-                inputBox.value = "";
-            } else if (buttonValue === "=") {
-                try {
-                    inputBox.value = eval(inputBox.value);
-                    resultDisplayed = true;
-                } catch (error) {
-                    inputBox.value = "Error";
-                    resultDisplayed = true;
-                }
-            } else {
-                updateInput(buttonValue);
-            }
-        });
-    });
-});
-
-let inputBox = document.getElementById('inputBox');
-
-function appendValue(value) {
-    inputBox.value += value;
+  }
+ 
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  getTasks();
 }
-
-function clearDisplay() {
-    inputBox.value = '';
+ 
+// Show To-Do List
+function getTasks() {
+ 
+  let tasks = JSON.parse(localStorage.getItem('tasks'));
+  let tasksView = document.getElementById('tasks');
+  tasksView.innerHTML = '';
+ 
+  for (let i = 0; i < tasks.length; i++) {
+    let title = tasks[i].title;
+    let description = tasks[i].description;
+ 
+    tasksView.innerHTML +=
+      `<div class="card mb-3">
+        <div class="card-body">
+        <div class="row">
+          <div class="col-sm-3 text-left">
+            <p>${title}</p>
+          </div>
+          <div class="col-sm-7 text-left">
+            <p>${description}</p>
+          </div>
+          <div class="col-sm-2 text-right">
+            <a href="#" onclick="deleteTask('${title}')" class="btn btn-danger ml-5">X</a>
+          </div>
+        </div>  
+       </div>
+      </div>`;
+  }
+ 
 }
-
-function deleteLast() {
-    inputBox.value = inputBox.value.slice(0, -1);
-}
-
-function calculate() {
-    let result = eval(inputBox.value);
-    inputBox.value = result;
-}
-
-function calculateSquareRoot() {
-    let inputValue = parseFloat(inputBox.value);
-    if (!isNaN(inputValue)) {
-        inputBox.value = Math.sqrt(inputValue);
-    } else {
-        inputBox.value = 'Invalid Input';
-    }
-}
+getTasks();
