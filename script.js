74ed7eb54dbbf8b11ff6ad2420b55c033da6231c
Rakshit honnavar <rakshithonnavar20@gmail.com>
2023-10-10 01:11:58 +0530

    OIBSIP changes

--- script.js
+++ script.js
@@ -1,77 +1,21 @@
-document.getElementById('form-Task').addEventListener('submit', saveTask);
- 
-// Save new To-Do
-function saveTask(e) {
- 
-  let title = document.getElementById('title').value;
-  let description = document.getElementById('description').value;
- 
- 
-  let task = {
-    title,
-    description
-  };
- 
-  if (localStorage.getItem('tasks') === null) {
-    let tasks = [];
-    tasks.push(task);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  } else {
-    let tasks = JSON.parse(localStorage.getItem('tasks'));
-    tasks.push(task);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }
- 
-  getTasks();
- 
-  // Reset form-Task
-  document.getElementById('form-Task').reset();
-  e.preventDefault();
- 
+const todoInput = document.querySelector(".todo-input");
+const todoInput = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+
+document.addEventListener("DOMContentLoaded",getLocalTodos);
+todoButton.addEventListener("click",addTodo);
+todoList.addEventListener("click",deleteCheck);
+filterOption.addEventListener("change",filterTodo);
+
+
+function addTodo(event)
+{
+  event.preventDefault();
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  const newTodo = document.createElement("li");
+  newTodo.innerText  = todoInput.value;
+  newTodo.classList.add("todo-item");
+
 }
- 
-// Delete To-Do 
-function deleteTask(title) {
- 
-  let tasks = JSON.parse(localStorage.getItem('tasks'));
-  for (let i = 0; i < tasks.length; i++) {
-    if (tasks[i].title == title) {
-      tasks.splice(i, 1);
-    }
-  }
- 
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-  getTasks();
-}
- 
-// Show To-Do List
-function getTasks() {
- 
-  let tasks = JSON.parse(localStorage.getItem('tasks'));
-  let tasksView = document.getElementById('tasks');
-  tasksView.innerHTML = '';
- 
-  for (let i = 0; i < tasks.length; i++) {
-    let title = tasks[i].title;
-    let description = tasks[i].description;
- 
-    tasksView.innerHTML +=
-      `<div class="card mb-3">
-        <div class="card-body">
-        <div class="row">
-          <div class="col-sm-3 text-left">
-            <p>${title}</p>
-          </div>
-          <div class="col-sm-7 text-left">
-            <p>${description}</p>
-          </div>
-          <div class="col-sm-2 text-right">
-            <a href="#" onclick="deleteTask('${title}')" class="btn btn-danger ml-5">X</a>
-          </div>
-        </div>  
-       </div>
-      </div>`;
-  }
- 
-}
-getTasks();
